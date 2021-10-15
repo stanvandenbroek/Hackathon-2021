87c681ef513610f49bc1cc462dd3b943708b1d67
Ben Krocké <benkrocke@hotmail.com>
2021-10-15 11:13:55 +0200

    Add graph

--- code/src/App.js
+++ code/src/App.js
@@ -1,29 +1,41 @@
+import React, { useEffect, useState } from "react";
 import * as Charts from "@ant-design/charts";
 import './App.css';
 import preData from "./data/pre-data.json";
 import postData from "./data/post-data.json";
-import { Layout, Menu, Space } from 'antd';
+import { Button, Layout, Menu, Row } from 'antd';
+const { DateTime } = require("luxon");
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content } = Layout;
+
+const JsonData = {
+  preData,
+  postData
+}
 
 function App() {
+  const [graphData, setGraphData] = useState('preData');
+  const [time, setTime] = useState(DateTime.now());
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(DateTime.now());
+    }, 5 * 1000);
+    return () => {
+      clearInterval(timer);
+    }
+  }, []);
+
   return (
-    <div className="App">
-        <Layout className="layout">
-          <Header>
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                <Menu.Item active key={1}>Home</Menu.Item>
-                <Menu.Item active key={2}>About</Menu.Item>
-            </Menu>
-          </Header>
-          <Content style={{ padding: '50px 50px' }}>
-            <Space direction="vertical">
-            <Charts.Line smooth={true} xField="time" yField="value" data={preData}></Charts.Line>
-            <Charts.Line smooth={true} xField="time" yField="value" data={postData}></Charts.Line>
-            </Space>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>,
+    <div className="p-12">
+      <div className="mb-12">
+        <div className="text-3xl text-pink-600 float-right">{time.toLocaleString(DateTime.TIME_24_SIMPLE)}</div>
+        <div className="text-3xl font-bold text-pink-600">Energieverbruik</div>
+        <div className="text-xl">Graafsewijk, 's-Hertogenbosch</div>
+      </div>
+
+      <Charts.Line className="mb-12" lineStyle={{ stroke: 'green' }} smooth={true} xField="time" yField="value" yMax data={JsonData[graphData]}></Charts.Line>
+      <Button type="primary" onClick={() => setGraphData('postData')}>Change Data</Button>
     </div>
   );
 }
